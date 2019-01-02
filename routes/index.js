a0332f316e6e79bdfc760a61b06f2ca5d5b103fe
pakasyk <mindaugas.krizonis@gmail.com>
2019-01-02 22:02:38 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/login', function(req, res, next) {
+  let user = fs.readFileSync('./database/users.json');
+  let userArray = JSON.parse(user);
+  res.render('login', { title: 'Login', users: userArray });
+});
+
+router.get('/register', function(req, res, next) {
+  let user = fs.readFileSync('./database/users.json');
+  let userArray = JSON.parse(user);
+  res.render('register', { title: 'Register', useris: userArray });
+});
+
+module.exports = router;
